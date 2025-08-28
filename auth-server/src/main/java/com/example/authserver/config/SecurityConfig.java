c364a0b70bfb0bffa504cfbda6de406072b0c962
slavalu <slavalu74@gmail.com>
2025-08-28 22:00:19 +0200

    small fix

--- auth-server/src/main/java/com/example/authserver/config/SecurityConfig.java
+++ auth-server/src/main/java/com/example/authserver/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.authserver;
+package com.example.authserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
